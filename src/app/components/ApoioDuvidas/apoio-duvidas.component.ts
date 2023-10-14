ec3dd7bff13c83a3500c4ea925556d77997065d5
JoaoGSAlves <joaog.silalves@gmail.com>
2023-10-14 03:09:35 -0300

    alteracoes de filtro/paginacao

--- src/app/components/ApoioDuvidas/apoio-duvidas.component.ts
+++ src/app/components/ApoioDuvidas/apoio-duvidas.component.ts
@@ -8,96 +8,115 @@ import { Usuario } from 'src/app/models/Usuario';
 import { AuthGuardService } from 'src/app/services/auth-guard.service';
 
 @Component({
-	selector: 'app-apoio-duvidas',
-	templateUrl: './apoio-duvidas.component.html',
-	styleUrls: ['./apoio-duvidas.component.css'],
+  selector: 'app-apoio-duvidas',
+  templateUrl: './apoio-duvidas.component.html',
+  styleUrls: ['./apoio-duvidas.component.css'],
 })
 export class ApoioDuvidasComponent implements OnInit {
-	busca: string;
-	chapterAssuntos: ChapterAssunto[];
-	chapterAssuntosTodos: ChapterAssunto[];
-	currentPage: number = 1;
-	itemsPerPage: number = 6;
-	startIndex: number = (this.currentPage - 1) * this.itemsPerPage;
-	endIndex: number = this.currentPage * this.itemsPerPage;
-	totalPages: number[];
-	usuario: Usuario = new Usuario();
-	idUsuarioLogado: string;
+  busca: string;
+  chapterAssuntos: ChapterAssunto[];
+  chapterAssuntosTodos: ChapterAssunto[];
+  currentPage: number = 1;
+  itemsPerPage: number = 6;
+  startIndex: number = (this.currentPage - 1) * this.itemsPerPage;
+  endIndex: number = this.currentPage * this.itemsPerPage;
+  totalPages: number[];
+  usuario: Usuario = new Usuario();
+  idUsuarioLogado: string;
 
-	constructor(
-		private _route: ActivatedRoute,
-		private chapterAssuntoService: ChapterAssuntoService,
-		private usuarioService: UsuariosService,
-		private authGuardService: AuthGuardService
-	) {}
+  constructor(
+    private _route: ActivatedRoute,
+    private chapterAssuntoService: ChapterAssuntoService,
+    private usuarioService: UsuariosService,
+    private authGuardService: AuthGuardService
+  ) {}
 
-	ngOnInit(): void {
-		this.chapterAssuntoService.ObterTodos().subscribe((data) => {
-			this.chapterAssuntosTodos = data;
-			this.chapterAssuntos = data;
-			this.calculateTotalPages(false);
-		});
+  ngOnInit(): void {
+    this.chapterAssuntoService.ObterTodos().subscribe((data) => {
+      this.chapterAssuntosTodos = data;
+      this.chapterAssuntos = data;
+      this.calculateTotalPages(false);
+    });
 
-		this.idUsuarioLogado = this.authGuardService.getIdUsuarioLogado();
+    this.idUsuarioLogado = this.authGuardService.getIdUsuarioLogado();
 
-		this.usuarioService.ObterUsuarioPorId(this.idUsuarioLogado).subscribe((resultado) => {
-			this.usuario = resultado;
-		});
-	}
-	previousPage() {
-		if (this.currentPage > 1) {
-			this.currentPage--;
-			this.updatePage();
-		}
-	}
+    this.usuarioService
+      .ObterUsuarioPorId(this.idUsuarioLogado)
+      .subscribe((resultado) => {
+        this.usuario = resultado;
+      });
+  }
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePage();
+    }
+  }
 
-	nextPage() {
-		const totalPages = Math.ceil(this.chapterAssuntos.length / this.itemsPerPage);
-		if (this.currentPage < totalPages) {
-			this.currentPage++;
-			this.updatePage();
-		}
-	}
+  nextPage() {
+    const totalPages = Math.ceil(
+      this.chapterAssuntos.length / this.itemsPerPage
+    );
+    if (this.currentPage < totalPages) {
+      this.currentPage++;
+      this.updatePage();
+    }
+  }
 
-	updatePage() {
-		this.startIndex = (this.currentPage - 1) * this.itemsPerPage;
-		this.endIndex = Math.min(this.currentPage * this.itemsPerPage, this.chapterAssuntos.length);
-	}
+  updatePage() {
+    this.startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    this.endIndex = Math.min(
+      this.currentPage * this.itemsPerPage,
+      this.chapterAssuntos.length
+    );
+  }
 
-	filtraPorTitulo(busca: string) {
-		this.chapterAssuntos = this.chapterAssuntosTodos.filter(
-			(value) => value.titulo.toLowerCase().includes(busca) || value.descricao.toLowerCase().includes(busca)
-		);
-		this.currentPage = 1;
-		this.calculateTotalPages(true);
-	}
+  filtraPorTitulo(busca: string) {
+    this.chapterAssuntos = this.chapterAssuntosTodos.filter(
+      (value) =>
+        value.titulo.toLowerCase().includes(busca) ||
+        value.descricao.toLowerCase().includes(busca)
+    );
+    this.currentPage = 1;
+    this.calculateTotalPages(true);
+  }
 
-	irParaPagina(i: number) {
-		this.currentPage = i;
-		this.updatePage();
-	}
+  irParaPagina(i: number) {
+    this.currentPage = i;
+    this.updatePage();
+  }
 
-	calculateTotalPages(filtrado: boolean) {
-		if (!filtrado) {
-			const itemsPerPage = this.itemsPerPage;
-			if (itemsPerPage > 0) {
-				const totalItems = this.chapterAssuntosTodos.length;
-				const pageCount = Math.ceil(totalItems / itemsPerPage);
+  calculateTotalPages(filtrado: boolean) {
+    if (!filtrado) {
+      const itemsPerPage = this.itemsPerPage;
+      if (itemsPerPage > 0) {
+        const totalItems = this.chapterAssuntosTodos.length;
+        const pageCount = Math.ceil(totalItems / itemsPerPage);
 
-				this.totalPages = Array.from({ length: pageCount }, (_, index) => index + 1);
-			} else {
-				console.error('O número de itens por página não está definido ou é inválido.');
-			}
-		} else {
-			const itemsPerPage = this.itemsPerPage;
-			if (itemsPerPage > 0) {
-				const totalItems = this.chapterAssuntos.length;
-				const pageCount = Math.ceil(totalItems / itemsPerPage);
+        this.totalPages = Array.from(
+          { length: pageCount },
+          (_, index) => index + 1
+        );
+      } else {
+        console.error(
+          'O número de itens por página não está definido ou é inválido.'
+        );
+      }
+    } else {
+      const itemsPerPage = this.itemsPerPage;
+      if (itemsPerPage > 0) {
+        const totalItems = this.chapterAssuntos.length;
+        const pageCount = Math.ceil(totalItems / itemsPerPage);
 
-				this.totalPages = Array.from({ length: pageCount }, (_, index) => index + 1);
-			} else {
-				console.error('O número de itens por página não está definido ou é inválido.');
-			}
-		}
-	}
+        this.totalPages = Array.from(
+          { length: pageCount },
+          (_, index) => index + 1
+        );
+      } else {
+        console.error(
+          'O número de itens por página não está definido ou é inválido.'
+        );
+      }
+    }
+  }
 }
